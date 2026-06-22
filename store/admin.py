a1aee9991b516090dc127ef1bf9b4808cb1cc90e
vawzenmaya <vawzenmaya@gmail.com>
2026-06-23 00:59:14 +0300

    Modified the UI, the models, added sub categories and sub sub categories in the models and also registered them with the admin

--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,34 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, SubCategory, SubSubCategory, Product
 
+# 1. The inline for Sub-SubCategories
+class SubSubCategoryInline(admin.TabularInline):
+    model = SubSubCategory
+    prepopulated_fields = {'slug': ('name',)}
+    extra = 1
+
+# 2. Register SubCategory to show the Sub-SubCategory inline
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [SubSubCategoryInline]
+
+# 3. The inline for SubCategories
+class SubCategoryInline(admin.TabularInline):
+    model = SubCategory
+    prepopulated_fields = {'slug': ('name',)}
+    extra = 1
+
+# 4. Register the main Category
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'slug')
+    inlines = [SubCategoryInline]
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock', 'category', 'modified_date', 'is_available')
     prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
 admin.site.register(Product, ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -4,10 +4,35 @@ class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(blank=True)
+    menu_image = models.ImageField(upload_to='photos/categories', blank=True, null=True)
 
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'subcategories'
+
+    def __str__(self):
+        return self.name
+
+# Add this new model
+class SubSubCategory(models.Model):
+    # Links directly to the SubCategory above it
+    subcategory = models.ForeignKey(SubCategory, related_name='subsubcategories', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'sub-subcategories'
 
     def __str__(self):
         return self.name
